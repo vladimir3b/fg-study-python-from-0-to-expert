501952e365edd8dce74d79453291a4563c24691d
Vladimir-Ioan <36692556+vladimirioan82@users.noreply.github.com>
2019-01-20 23:36:16 +0200

    starting learning

---
+++ 01 - Starting/01 - Python Type Hierarchy.py
@@ -0,0 +1,10 @@
+'''
+
+  Lesson 005 - The Python Type Hierarchy
+    based on the course - Python 3: Deep Dive (Part 1) by Fred Baptiste
+
+      Main Course address: https://www.udemy.com/python-3-deep-dive-part-1/
+      Current lesson address: https://www.udemy.com/python-3-deep-dive-part-1/learn/v4/t/lecture/7192358
+
+'''
+
